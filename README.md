6661aaec5cd5cf13741bf7092392e698a3b126aa
hhl <hhl@192.168.10.47>
2022-11-28 16:34:09 +0800

    delete console

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# zce-gulp-demo
+# hhl-gulp-demo
+# 自动化构建的脚手架
+# 安装使用需要在 其项目中新建 hhl-pages-config
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-// console.log('cli index.js')
-// // 执行入口  引用则会执行
-// console.log(process.argv)
 
-// // 设置当前跑的是当前的目录
+// 执行入口  引用则会执行
+// 设置当前跑的是当前的目录
 process.argv.push('--cwd')
 process.argv.push(process.cwd())
 
@@ -11,7 +9,6 @@ process.argv.push(process.cwd())
 process.argv.push('--gulpfile')
 // 该方法会将相对路径拼接生成一个绝对路径，并自动检验该路径是否存在, 会自动去找找到package的main字段
 process.argv.push(require.resolve('./'))
-console.log(process.argv)
 // 先找到执行gulp-cli的执行
 require('.bin/gulp')
 
